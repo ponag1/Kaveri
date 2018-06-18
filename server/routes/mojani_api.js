be03e3cafee009f0aee3c42b8a76a9cd2f0d0963
ponag1 <38813991+ponag1@users.noreply.github.com>
2018-06-18 15:58:03 +0530

    hyper

--- server/routes/mojani_api.js
+++ server/routes/mojani_api.js
@@ -1,3 +1,4 @@
+var requestify = require('requestify'); 
 var express = require('express');
 var router = express.Router();
 var vcapServices = require("vcap_services");
@@ -36,6 +37,72 @@ router.get('/api/getLandRecordsMojaniByPid/:id', (req, res) => {
 	let sketchURL;
   console.log('Inside Express api to get land records by Pid');
 	if(!isNaN(req.params.id)){
+		mojani.find({selector:{pid:Number(req.params.id)}}, function(er, result) {
+			if (er) {
+			console.log("Error finding documents");
+			res.json({success : false,message:"Error finding documents: "+er,landRecords:null});
+			}
+	  console.log('Found documents with PID count:'+ req.params.id +":"+ result.docs.length);
+ 
+	  if(result.docs.length > 0){
+			console.log('Found documents with PID count:'+ req.params.id +":"+ result.docs.length);
+			let doc = result.docs[0];
+			let attachmentName;
+			if(doc['_attachments'] !=null){
+				attachmentName = Object.keys(doc['_attachments'])[0];
+				sketchURL= cloudant_url + "/" + mojaniDBName + "/" + doc["_id"]+ "/" + attachmentName;
+			}
+		}
+		else
+			res.json({success : true, message:"No documents found", landRecords:null});
+		});
+		
+	var land_details;
+	requestify.get('http://13.232.73.187:3000/api/org.bhoomi.landrecords.LandRecord/'+req.params.id).then(function(response) {
+	// Get the response body
+	land_details = response.getBody();
+	var owner = land_details.owner;
+	var owner_details;
+	owner = owner.split("#")[1];
+		requestify.get('http://13.232.73.187:3000/api/org.bhoomi.landrecords.Owner/'+owner).then(function(response) {
+		// Get the response body
+		owner_details = response.getBody();
+			var landRecordData = {
+				pid : land_details.pid,
+				wardNo : land_details.wardNo,
+				areaCode : land_details.areaCode,
+				siteNo : land_details.siteNo,
+				txnId : land_details.txnId,
+				timeStamp : land_details.timeStamp,
+				isMojaniApproved : land_details.isMojaniApproved,
+				isKaveriApproved : land_details.isKaveriApproved,
+				geoData : {
+					latitude : land_details.latitude,
+					longitude : land_details.longitude,
+					length : land_details.length,
+					width : land_details.width,
+					totalArea: land_details.totalArea,
+					address : land_details.address,
+				},
+				ownerDetails :{
+					aadharNo : owner_details.aadharNo,
+					ownerName : owner_details.ownerName,
+					gender : owner_details.gender,
+					mobileNo : owner_details.mobileNo,
+					emailID : owner_details.emailID,
+					address : owner_details.address
+				}
+
+		}
+			res.json({success : true, message:"Found "+result.docs.length+" documents", landRecords: landRecordData, sketchURL : sketchURL});
+		);
+	});
+	};
+	
+ 
+	
+	/*
+	
   	mojani.find({selector:{pid:Number(req.params.id)}}, function(er, result) {
 			if (er) {
 			console.log("Error finding documents");
@@ -55,7 +122,7 @@ router.get('/api/getLandRecordsMojaniByPid/:id', (req, res) => {
 		}
 		else
 			res.json({success : true, message:"No documents found", landRecords:null});
-		});
+		}); */
 	}else {
 		res.json({success : false, message:"PID sent null in request", landRecords:null});
 	}
