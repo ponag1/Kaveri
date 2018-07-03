b15f3c1fcc2869ab0b309840bc11f5ca0b7aac55
ponag1 <38813991+ponag1@users.noreply.github.com>
2018-07-03 22:11:17 +0530

    Update registration-form.component.ts

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -140,7 +140,14 @@ export class RegistrationFormComponent implements OnInit {
             response => {
               console.log("res received getLandRecordbyPid service" + JSON.stringify(response));
               if (response !=null && response.success) {
-                this.landRecord = <LandRecord> response.landRecords;               
+                this.landRecord = <LandRecord> response.landRecords;  
+                this.landRecord.newOwnerDetails = <Owner> this.landRecord.ownerDetails;
+                this.landRecord.ownerDetails = <Owner> null;
+                this.landRecord.ownerDetails.aadharNo = 0;
+                this.landRecord.ownerDetails.address = "NA";
+                this.landRecord.ownerDetails.emailID = "NA";
+                this.landRecord.ownerDetails.mobileNo = 0;
+                this.landRecord.ownerDetails.ownerName = "LAND AUTHORITY";
                 console.log("landRecord object received:" + this.landRecord);
                 if(response.sketchURL!=null && response.sketchURL!=""){
                   this.sketchURL = response.sketchURL;
